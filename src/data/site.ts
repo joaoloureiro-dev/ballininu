1a333b9d3592bca5a5214cf771a74bf9d3d63a55
Joao Loureiro <joaoloureiro.dev@gmail.com>
2026-09-24 13:12:18 +0100

    chore: initialize BALLININU design foundation

---
+++ src/data/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: 'BALLININU',
+    ticker: '$BALLININU',
+
+    tagline: 'More than a meme. It’s a lifestyle.',
+
+    contractAddress: '',
+
+    links: {
+        telegram: '',
+        x: '',
+        buy: '',
+    },
+}
